8dbd12ef3944b543797891d666bd4a10221ef300
SvetlanaMelekhova <71405460+SvetlanaMelekhova@users.noreply.github.com>
2021-10-21 17:05:04 +0300

    start injected Dagger

--- app/src/main/java/by/svetlana/project/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/by/svetlana/project/spotifyclone/ui/MainActivity.kt
@@ -1,9 +1,18 @@
-package by.svetlana.project.spotifyclone
+package by.svetlana.project.spotifyclone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.svetlana.project.spotifyclone.R
+import com.bumptech.glide.RequestManager
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
