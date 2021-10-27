e0242accbf2a707a0e8a4430087724ee043356df
SvetlanaMelekhova <71405460+SvetlanaMelekhova@users.noreply.github.com>
2021-10-27 07:43:25 +0300

    add classes for callbacks, MusicServiceConnection and event. Add some logic in FirebaseMusicSource, MusicService.

--- app/src/main/java/by/svetlana/project/spotifyclone/other/Constants.kt
+++ app/src/main/java/by/svetlana/project/spotifyclone/other/Constants.kt
@@ -5,4 +5,9 @@ object Constants {
     const val  SONG_COLLECTION = "songs"
     const val  NOTIFICATION_ID = 1
     const val  NOTIFICATION_CHANNEL_ID = "music"
+
+    const val NETWORK_ERROR = "NETWORK_ERROR"
+
+    const val  MEDIA_ROOT_ID = "root_id"
+
 }
